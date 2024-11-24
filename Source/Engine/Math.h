cdf649a0d71e306a56fb4ee15372bafe76514e37
Snuskasaurus <julien.rogel@gmail.com>
2024-11-24 15:36:32 +0100

    added a define for FORCE_INLINE

--- Source/Engine/Math.h
+++ Source/Engine/Math.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#define FORCE_INLINE __forceinline
+
 #include <DirectXMath.h>
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -8,7 +10,7 @@ struct TVector2f
     float x = 0.0f;
     float y = 0.0f;
 
-    __forceinline static float Dot(const TVector2f&  v1, const TVector2f&  v2)
+    FORCE_INLINE static float Dot(const TVector2f&  v1, const TVector2f&  v2)
     {
         return v1.x * v2.x + v1.y * v2.y;
     }
@@ -20,56 +22,56 @@ struct TVector3f
     float y = 0.0f;
     float z = 0.0f;
 
-    __forceinline bool IsNormalized()
+    FORCE_INLINE bool IsNormalized()
     {
         float squareLength = TVector3f::SquareLength(*this);
         return squareLength <= 1.0f - FLT_EPSILON && squareLength >= 1.0f + FLT_EPSILON;
     }
-    __forceinline static float Dot(const TVector3f&  v1, const TVector3f&  v2)
+    FORCE_INLINE static float Dot(const TVector3f&  v1, const TVector3f&  v2)
     {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
-    __forceinline static TVector3f Cross(const TVector3f&  v1, const TVector3f&  v2)
+    FORCE_INLINE static TVector3f Cross(const TVector3f&  v1, const TVector3f&  v2)
     {
         return { };
     }
-    __forceinline static float SquareLength(const TVector3f&  v1)
+    FORCE_INLINE static float SquareLength(const TVector3f&  v1)
     {
         return v1.x + v1.y + v1.z;
     }
-    __forceinline static float Length(const TVector3f&  v1)
+    FORCE_INLINE static float Length(const TVector3f&  v1)
     {
         return sqrtf(SquareLength(v1));
     }
-    __forceinline static TVector3f Normalize(const TVector3f&  v1)
+    FORCE_INLINE static TVector3f Normalize(const TVector3f&  v1)
     {
         float Length = TVector3f::Length(v1);
         return {v1.x / Length, v1.y / Length, v1.z / Length };
     }
-     __forceinline TVector3f operator-() const
+     FORCE_INLINE TVector3f operator-() const
     {
         return { -x, -y, -z };
     }
-    __forceinline TVector3f& operator+=(const TVector3f& rhs)
+    FORCE_INLINE TVector3f& operator+=(const TVector3f& rhs)
     {
         this->x += rhs.x;
         this->y += rhs.y;
         this->z += rhs.z;
         return *this;
     }
-    __forceinline TVector3f& operator-=(const TVector3f& rhs)
+    FORCE_INLINE TVector3f& operator-=(const TVector3f& rhs)
     {
         this->x -= rhs.x;
         this->y -= rhs.y;
         this->z -= rhs.z;
         return *this;
     }
-    __forceinline friend TVector3f operator+(TVector3f lhs, const TVector3f& rhs)
+    FORCE_INLINE friend TVector3f operator+(TVector3f lhs, const TVector3f& rhs)
     {
         lhs += rhs;
         return lhs;
     }
-    __forceinline friend TVector3f operator-(TVector3f lhs, const TVector3f& rhs)
+    FORCE_INLINE friend TVector3f operator-(TVector3f lhs, const TVector3f& rhs)
     {
         lhs += rhs;
         return lhs;
@@ -83,7 +85,7 @@ struct TVector4f
     float z = 0.0f;
     float w = 0.0f;
 
-    inline static float Dot(TVector4f v1, TVector4f v2)
+    FORCE_INLINE static float Dot(TVector4f v1, TVector4f v2)
     {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w;
     }
@@ -91,22 +93,22 @@ struct TVector4f
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 struct alignas(16) TMatrix4f
 {
-    TVector4f x;
-    TVector4f y;
-    TVector4f z;
-    TVector4f w;
+    TVector4f x = { 0.0f, 0.0f, 0.0f, 0.0f };
+    TVector4f y = { 0.0f, 0.0f, 0.0f, 0.0f };
+    TVector4f z = { 0.0f, 0.0f, 0.0f, 0.0f };
+    TVector4f w = { 0.0f, 0.0f, 0.0f, 0.0f };
 
     static const TMatrix4f Identity;
 
-    __forceinline static TMatrix4f MatrixTranslation(TVector3f translation);
-    __forceinline static TMatrix4f MatrixRotationX(float angle); 
-    __forceinline static TMatrix4f MatrixRotationY(float angle); 
-    __forceinline static TMatrix4f MatrixRotationZ(float angle); 
-    __forceinline static TMatrix4f MatrixScale(float scale);
-    __forceinline static TMatrix4f MatrixScaleUniform(float scale);
-    __forceinline static TMatrix4f MatrixLookAtRH(TVector3f cameraPosition, TVector3f lookAtPosition, TVector3f up);
+    FORCE_INLINE static TMatrix4f MatrixTranslation(TVector3f translation);
+    FORCE_INLINE static TMatrix4f MatrixRotationX(float angle); 
+    FORCE_INLINE static TMatrix4f MatrixRotationY(float angle); 
+    FORCE_INLINE static TMatrix4f MatrixRotationZ(float angle); 
+    FORCE_INLINE static TMatrix4f MatrixScale(float scale);
+    FORCE_INLINE static TMatrix4f MatrixScaleUniform(float scale);
+    FORCE_INLINE static TMatrix4f MatrixLookAtRH(TVector3f cameraPosition, TVector3f lookAtPosition, TVector3f up);
 
-    __forceinline static TMatrix4f Transpose(const TMatrix4f& m)
+    FORCE_INLINE static TMatrix4f Transpose(const TMatrix4f& m)
     {
         return
         {
@@ -117,7 +119,7 @@ struct alignas(16) TMatrix4f
        };
     }
     
-    __forceinline TMatrix4f& operator*=(const TMatrix4f& rhs)
+    FORCE_INLINE TMatrix4f& operator*=(const TMatrix4f& rhs)
     {
         const TMatrix4f m1 = *this;
         const TMatrix4f m2 = TMatrix4f::Transpose(rhs);
@@ -127,7 +129,7 @@ struct alignas(16) TMatrix4f
         this->w = { TVector4f::Dot(m1.w, m2.x),    TVector4f::Dot(m1.w, m2.y),    TVector4f::Dot(m1.w, m2.z),    TVector4f::Dot(m1.w, m2.w) };
         return *this;
     }
-    __forceinline friend TMatrix4f operator*(TMatrix4f lhs, const TMatrix4f& rhs)
+    FORCE_INLINE friend TMatrix4f operator*(TMatrix4f lhs, const TMatrix4f& rhs)
     {
         lhs *= rhs;
         return lhs;
